94c68fd800f441aea13e686f3fa87ef3f2b9bf28
kyleshao <roger20001016@gmail.com>
2021-12-23 23:21:42 +0800

    Recommit

--- app.py
+++ app.py
@@ -33,74 +33,74 @@ parser = WebhookParser(channel_secret)
 
 
 
-# @app.route("/callback", methods=["POST"])
-# def callback():
-#     signature = request.headers["X-Line-Signature"]
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-#     # parse webhook body
-#     try:
-#         events = parser.parse(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-#     # if event is MessageEvent and message is TextMessage, then echo text
-#     for event in events:
-#         if not isinstance(event, MessageEvent):
-#             continue
-#         if not isinstance(event.message, TextMessage):
-#             continue
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
 
-#         line_bot_api.reply_message(
-#             event.reply_token, TextSendMessage(text=event.message.text)
-#         )
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text)
+        )
 
-#     return "OK"
-
-
-# @app.route("/webhook", methods=["POST"])
-# def webhook_handler():
-#     signature = request.headers["X-Line-Signature"]
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info(f"Request body: {body}")
-
-#     # parse webhook body
-#     try:
-#         events = parser.parse(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
-
-#     # if event is MessageEvent and message is TextMessage, then echo text
-#     for event in events:
-#         if not isinstance(event, MessageEvent):
-#             continue
-#         if not isinstance(event.message, TextMessage):
-#             continue
-#         if not isinstance(event.message.text, str):
-#             continue
-#         # print(f"\nFSM STATE: {machine.state}")
-#         # print(f"REQUEST BODY: \n{body}")
-#         # Create a machine for new user
-#         if event.source.user_id not in machines:
-#             machines[event.source.user_id] = create_machine()
-
-#         # Advance the FSM for each MessageEvent
-#         response = machines[event.source.user_id].advance(event)
-#         if response == False:
-#             send_text_message(event.reply_token, "Invalid Command !")
-
-#     return "OK"
+    return "OK"
 
 
-# # @app.route("/show-fsm", methods=["GET"])
-# # def show_fsm():
-# #     machine.get_graph().draw("fsm.png", prog="dot", format="png")
-# #     return send_file("fsm.png", mimetype="image/png")
+@app.route("/webhook", methods=["POST"])
+def webhook_handler():
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info(f"Request body: {body}")
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+        if not isinstance(event.message.text, str):
+            continue
+        # print(f"\nFSM STATE: {machine.state}")
+        # print(f"REQUEST BODY: \n{body}")
+        # Create a machine for new user
+        if event.source.user_id not in machines:
+            machines[event.source.user_id] = create_machine()
+
+        # Advance the FSM for each MessageEvent
+        response = machines[event.source.user_id].advance(event)
+        if response == False:
+            send_text_message(event.reply_token, "Invalid Command !")
+
+    return "OK"
 
 
-# if __name__ == "__main__":
-#     port = os.environ.get("PORT", 8000)
-#     app.run(host="0.0.0.0", port=port, debug=True)
+# @app.route("/show-fsm", methods=["GET"])
+# def show_fsm():
+#     machine.get_graph().draw("fsm.png", prog="dot", format="png")
+#     return send_file("fsm.png", mimetype="image/png")
+
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT", 8000)
+    app.run(host="0.0.0.0", port=port, debug=True)
